2d1409e24f73e50645de68229c4d15b8a415758c
Henry Lou <henrylou.st@gmail.com>
2025-05-02 15:37:47 +0200

    added new schemas and updated data validation method in routes

--- src/models.py
+++ src/models.py
@@ -5,5 +5,5 @@ class Message(db.Model):
     id = db.Column(db.String, primary_key=True)
     recipient = db.Column(db.String, nullable=False)
     content = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.now())
+    timestamp = db.Column(db.DateTime, default=datetime.now)
     read = db.Column(db.Boolean, default=False)
--- src/schemas.py
+++ src/schemas.py
@@ -1,8 +1,18 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 class MessageSchema(Schema):
     id = fields.Str(dump_only=True)
-    recipient = fields.Str(required=True)
+    recipient = fields.Email(required=True)
     content = fields.Str(required=True)
-    timestamp = fields.DateTime()
-    read = fields.Bool()
+    timestamp = fields.DateTime(dump_only=True)
+    read = fields.Bool(dump_only=True)
+
+class RecipientSchema(Schema):
+    recipient = fields.Email(required=True)
+
+class PaginationSchema(Schema):
+    start = fields.Int(missing=None, validate=validate.Range(min=0))
+    stop = fields.Int(missing=None, validate=validate.Range(min=0))
+
+class BulkDeleteSchema(Schema):
+    ids = fields.List(fields.Str(), required=True)
